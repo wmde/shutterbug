d28355360f4a81a48d3a12d623a4ba4d63118b33
Abban Dunne <abban.dunne@wikimedia.de>
2020-05-18 09:50:31 +0200

    Add basic overview styles

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-overview">
-        <h1>Campaign overview</h1>
+        <h1 class="page-overview-heading">Campaign overview</h1>
         <div v-if="lastError" class="error">
             <h2 class="error__title">Error</h2>
             <div class="error__message">There was an error loading the overview.</div>
@@ -11,11 +11,12 @@
                 class="folders__item folder"
             >
                 <router-link
-                        class="folder__title"
+                        class="folder__card"
                         :to="`/slides/${folder.campaign}`">
-                    {{ folder.campaign }}
+                    <Icon :icon="'folder'" class="folder__icon"></Icon>
+                    {{ folder.campaign }}<br>
+                    <i class="folder__info" >( {{ folder.testCaseCount }} tests )</i>
                 </router-link>
-                <span class="folder__info" >( {{ folder.testCaseCount }} tests )</span>
             </li>
         </ol>
         <div v-else-if="!lastError">Loading overview ...</div>
@@ -24,9 +25,13 @@
 
 <script lang="ts">
     import {defineComponent, reactive, toRefs, watchEffect} from "@vue/composition-api";
+    import Icon from "@/components/Icon.vue";
 
     export default defineComponent( {
         name: "Overview",
+        components: {
+            Icon
+        },
         setup() {
             const overviewStateInit = {
                 isLoading: true,
@@ -61,31 +66,65 @@
     .page-overview {
         margin-top: $header-height;
         padding: $header-height;
-    }
 
-    .folders {
-        margin: 0;
-    }
-
-    .folder {
-        list-style: none;
-        margin: 0;
-        padding: .25em 0;
-
-        &__title {
-            text-decoration: none;
-            color: $font-color-dark;
-            font-size: 125%;
+        &-heading {
+            margin-bottom: $header-height;
         }
 
-        &__title:hover {
-            text-decoration: underline;
+        .folders {
+            margin: 0 (-$stage-gutter / 2);
+            display: flex;
+            flex-wrap: wrap;
+            align-content: stretch;
+            height: 100%;
+
+            &__item {
+                width: 100%;
+
+                @media (min-width: 950px) {
+                    width: 50%;
+                }
+            }
         }
 
-        &__info {
-            padding-left: 10px;
+        .folder {
+            list-style: none;
+            margin: 0;
+            line-height: 1.2;
+
+            &__card {
+                position: relative;
+                display: block;
+                text-decoration: none;
+                color: $font-color-dark;
+                font-size: 125%;
+                margin: 0 ($stage-gutter / 2) 40px;
+                padding: 20px 20px 20px 80px;
+                border: 1px solid $border-color;
+
+                &:hover, &:focus {
+                    border-color: $overview-border-hover;
+                }
+            }
+
+            &__icon {
+                position: absolute;
+                top: 50%;
+                left: 18px;
+                height: 50px;
+                width: 50px;
+                margin-top: -25px;
+            }
+
+            &__title:hover {
+                text-decoration: underline;
+            }
+
+            &__info {
+                color: $font-color-secondary;
+                font-size: 14px;
+            }
         }
     }
-
 
 </style>
