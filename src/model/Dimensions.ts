fb40a92efb023bf2f39b6117d5db31ddf4196b46
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-09-08 10:02:37 +0200

    Prepare for combined dimension 'platform'
    The dimensions browser and OS were combined in the banner-shot application.
    Shutterbug got adjusted to be able to visualize the new dimension.
    
    https://phabricator.wikimedia.org/T260039

--- src/model/Dimensions.ts
+++ src/model/Dimensions.ts
@@ -1,10 +1,9 @@
 // TODO maybe use types instead of string constants here
 export const BANNER = 'banner';
-export const BROWSER = 'browser';
 export const DEVICE = 'device';
-export const OPERATING_SYSTEM = 'operating_system';
+export const PLATFORM = 'platform';
 export const ORIENTATION = 'orientation';
 export const RESOLUTION = 'resolution';
-export const ALLOWED_DIMENSIONS = [BANNER, BROWSER, DEVICE, OPERATING_SYSTEM, ORIENTATION, RESOLUTION];
+export const ALLOWED_DIMENSIONS = [BANNER, DEVICE, PLATFORM, ORIENTATION, RESOLUTION];
 
 export type DimensionMap = Map<string,string[]>
